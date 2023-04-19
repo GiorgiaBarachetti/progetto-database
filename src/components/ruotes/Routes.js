f4ffd2cfd2718d9a7d96ff050f4ca70c99ce8a09
simone03 <simoneairaghi03@gmai.com>
2023-04-19 14:11:05 +0200

    h

--- src/components/ruotes/Routes.js
+++ src/components/ruotes/Routes.js
@@ -6,12 +6,12 @@ import Registration from "../LoginandRegistration/Registration";
 
 const Routers = () => {
     
-    //<Route path={ROUTE.timbro} element={<Timbro/>}/>
     return (
         <BrowserRouter>
         <Routes>
             <Route path={ROUTE.login} element={<Login/>}/>
             <Route path={ROUTE.registration} element={<Registration/>}/>
+            <Route path={ROUTE.timbro} element={<Timbro/>}/>
         </Routes>
         
         </BrowserRouter>
